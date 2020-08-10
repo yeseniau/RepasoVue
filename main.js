bd43f6e4f428c2725efffd741f03a1d9b801b583
yeseniau <urdaneta.yesenia@gmail.com>
2020-08-10 19:49:34 -0400

    refactorizar variantes

--- main.js
+++ main.js
@@ -2,38 +2,44 @@
 var app = new Vue({
   el: '#app',
   data: {
-    descrip: 'lorem ipsum',
+    descrip: 'lorem ipsum lorem ipsum lorem ipsum',
     product: 'Longaniza de Brocolli',
-    img: './assets/img/longaniza.jpg',
+    selectedVariant: {},
     link: './longaniza.html',
-    stock: 12,
     details: ["500 gr", "organica", "libre de colesterol"],
-    variantes: [
+    variants: [
       {
         id: 5,
         type: 'red',
         img: './assets/img/longaniza.jpg',
+        stock: 12,
+        default: false
       },
       {
         id: 4,
         type: 'white',
         img: './assets/img/longablanca.png',
+        stock: 5,
+        default: true
       }],
     cart: 0
   },
 
+
+
   methods: {
-    updateImg(imagen) {
-      this.img = imagen
+
+    updateProduct(variant) {
+      this.selectedVariant = variant
     },
     addToCart() {
       this.cart += 1
-      this.stock -= 1
+      this.selectedVariant.stock -= 1
     },
     removeFromCart() {
       if (this.cart > 0) {
         this.cart -= 1
-        this.stock += 1
+        this.selectedVariant.stock += 1
       }
     },
   },
@@ -41,5 +47,17 @@ var app = new Vue({
     inStock() {
       return this.stock > 0 ? true : false // el signo de interrogacion representa el ciclo iF se llama operador ternario, y los dos puntos son representan ELSE
     },
-  }
+    img() {
+      return this.selectedVariant.img
+    },
+    stock() {
+      return this.selectedVariant.stock
+    }
+  },
+
+  created() {
+    this.selectedVariant = this.variants.find(item => item.default == true)
+
+  },
+
 })
